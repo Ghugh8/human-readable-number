3e0647970511da3ebb4c44729410726a2a27d892
ghugh8 <ghugh87@gmail.com>
2022-07-16 21:12:41 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ module.exports = function toReadable (number) {
     const ty = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
     const hundr = ['one hundred', 'two hundred', 'three hundred', 'four hundred', 'five hundred', 'six hundred', 'seven hundred', 'eight hundred', 'nine hundred'];
     const array = Array.from(String(number), Number);
-    let arr = (String(number).length === 3) ? array : array.unshift(0);
+    let arr = (String(number).length === 3) ? array : (String(number).length === 2) ? array.unshift(0) : array.unshift(0, 0);
+    
     console.log(array, arr)
   
     let a = array[0];
@@ -13,13 +14,28 @@ module.exports = function toReadable (number) {
     let d = (10 * b + c);
     console.log(a, b, c, d);
   
-    if (a != 0 && b === 0 && c ===0) {
+    if (a != 0 && b === 0 && c === 0) {
       return hundr[a-1]} else if (a != 0 && b === 0) {
-          return hundr[a-1] + ' ' + digit[c-1]} else if (a != 0 && c === 0) {
-              return hundr[a-1] + ' ' + ty[b-1]} else if (a===0 && d > 20) {
-                return ty[b-1] + ' ' + digit[c-1];} else if (a===0 && d > 10 && d < 20) {
-                  return teen[c-1]} else if (a===0 && d < 10) {
-                    return digit[c-1]} else if (c != 0 && b != 0 && d > 10 && d < 20) {
-                      return hundr[a-1] + ' ' + teen[c-1]} else {
-                        return hundr[a-1] + ' ' + ty[b-1] + ' ' + digit[c-1];}
+          return hundr[a-1] + ' ' + digit[c-1]} 
+          
+          else if (a != 0 && c === 0) {
+              return hundr[a-1] + ' ' + ty[b-1]} 
+              
+              else if (a === 0 && d > 20 && (d % 10) != 0) {
+                return ty[b-1] + ' ' + digit[c-1];} 
+                
+                else if (a === 0 && (d % 10) == 0) {
+                  return ty[b-1]}   
+                                        
+                    else if (a===0 && d > 10 || d < 20) {
+                      return teen[c-1]} 
+                      
+                      else if (a===0 && b === 0 && d < 10) {
+                        return digit[c-1]} 
+                        
+                        else if (c != 0 && b != 0 && d > 10 && d < 20) {
+                          return hundr[a-1] + ' ' + teen[c-1]} 
+                          
+                          else {
+                            return hundr[a-1] + ' ' + ty[b-1] + ' ' + digit[c-1];}
   }
